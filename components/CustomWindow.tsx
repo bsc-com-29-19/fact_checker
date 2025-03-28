37b67c3446523fa1fbce07e99722278f6f104df7
bed-com-07-20 <bed-com-07-20@unima.ac.mw>
2025-03-29 00:52:02 +0200

    Type section: removed the one on main page to prevent redundancy

--- components/CustomWindow.tsx
+++ components/CustomWindow.tsx
@@ -1,11 +1,9 @@
 //CustomWindow.tsx
 "use client";
 
-import {
-  WindowProps,
-  useChatContext,
-} from "@copilotkit/react-ui";
+import { WindowProps, useChatContext } from "@copilotkit/react-ui";
 import "@copilotkit/react-ui/styles.css";
+
 export default function CustomWindow({ children }: WindowProps) {
   const { open, setOpen } = useChatContext();
 
@@ -13,15 +11,24 @@ export default function CustomWindow({ children }: WindowProps) {
 
   return (
     <div
-      className=" fixed right-0 top-0 h-full w-1/3 "
+      className="fixed right-0 top-0 h-full w-1/3"
       onClick={() => setOpen(false)}
     >
       <div
         className="bg-white rounded-lg shadow-xl max-w-2xl w-full h-full overflow-auto"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="flex flex-col h-full">{children}</div>
+        <div className="flex flex-col h-full">
+          {children}
+        </div>
       </div>
+
+      {/* Hide the CopilotKit input field */}
+      <style jsx global>{`
+        .copilotkit-input-container {
+          display: none !important;
+        }
+      `}</style>
     </div>
   );
 }
