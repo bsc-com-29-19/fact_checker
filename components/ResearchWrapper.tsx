ce415dfb19e724df876d3f0d22155eb1b0bcc152
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-04-18 16:22:04 +0200

    Tabs : add tabs for results and sources

--- components/ResearchWrapper.tsx
+++ components/ResearchWrapper.tsx
@@ -34,7 +34,7 @@ export function ResearchWrapper() {
             rel="noopener noreferrer"
             className="text-slate-600 font-medium hover:underline"
           >
-            Powered by Mayamiko 🪁
+            Powered by Fact Checker MW
           </a>
         </footer>
       </div>
--- components/ResultsView.tsx
+++ components/ResultsView.tsx
@@ -5,103 +5,149 @@ import { useResearchContext } from "@/lib/research-provider";
 import { motion } from "framer-motion";
 import { BookOpenIcon, LoaderCircleIcon, SparkleIcon } from "lucide-react";
 // import { SkeletonLoader } from "./SkeletonLoader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useCoAgent } from "@copilotkit/react-core";
-import { Progress } from "./Progress";
+import { Progress } from "./progress";
 import { AnswerMarkdown } from "./AnswerMarkdown";
 import { useModel } from "@/contexts/modelContext";
 import { useAgent } from "@/contexts/agentContext";
 import { AgentState } from "@/lib/types";
 
 export function ResultsView() {
-    const { researchQuery } = useResearchContext();
-    const {model} = useModel();
-    const {agent} = useAgent();
-    //agent state
-    const { state: agentState } = useCoAgent<AgentState>({
-        name: agent,
-        initialState:{
-            model,
-        }
-    });
+  const { researchQuery } = useResearchContext();
+  const { model } = useModel();
+  const { agent } = useAgent();
+  //agent state
+  const { state: agentState } = useCoAgent<AgentState>({
+    name: agent,
+    initialState: {
+      model,
+    },
+  });
 
-    console.log("AGENT_STATE", agentState);
+  console.log("AGENT_STATE", agentState);
 
-    //keeps track of the current agent processing state
-    const steps =
-        agentState?.steps?.map((step: any) => {
-            return {
-                description: step.description || "",
-                status: step.status || "pending",
-                updates: step.updates || [],
-            };
-        }) || [];
+  const tabs: { name: string; value: string }[] = [
+    {
+      name: "Results",
+      value: "results",
+    },
+    {
+      name: "Sources",
+      value: "sources",
+    },
+  ];
 
-    const isLoading = !agentState?.answer?.markdown;
+  //keeps track of the current agent processing state
+  const steps =
+    agentState?.steps?.map((step: any) => {
+      return {
+        description: step.description || "",
+        status: step.status || "pending",
+        updates: step.updates || [],
+      };
+    }) || [];
 
-    return (
-        <motion.div
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -50 }}
-            transition={{ duration: 0.5, ease: "easeOut" }}
-        >
-            <div className='max-w-[1000px] p-8 lg:p-4 flex flex-col gap-y-8 mt-4 lg:mt-6 text-sm lg:text-base'>
-                <div className='space-y-4'>
-                    <h1 className='text-3xl lg:text-4xl font-extralight'>
-                        {researchQuery}
-                    </h1>
-                </div>
+  const isLoading = !agentState?.answer?.markdown;
 
-                <Progress steps={steps} />
+  console.log("is loading :", isLoading);
 
-                <div className='grid grid-cols-12 gap-8'>
-                    <div className='col-span-12 lg:col-span-8 flex flex-col'>
-                        <h2 className='flex items-center gap-x-2'>
-                            {isLoading ? (
-                                <LoaderCircleIcon className='animate-spin w-4 h-4 text-slate-500' />
-                            ) : (
-                                <SparkleIcon className='w-4 h-4 text-slate-500' />
-                            )}
-                            Answer
-                        </h2>
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -50 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+    >
+      {/* <div className="max-w-[1000px] p-8 lg:p-4 flex flex-col gap-y-8 mt-4 lg:mt-6 text-sm lg:text-base">
+        <div className="space-y-4">
+          <h1 className="text-3xl lg:text-4xl font-extralight">
+            {researchQuery}
+          </h1>
+        </div>
 
-                        <div className='text-slate-500 font-light'>
-                            {isLoading ? 
-                            // (
-                            //     <SkeletonLoader />
-                            // )
-                            'Loading....'
-                             : (
-                                <AnswerMarkdown markdown={agentState?.answer?.markdown} /> // displays search results
-                            )}
-                        </div>
-                    </div>
+        <Progress steps={steps} />
 
-                    {agentState?.answer?.references?.length && (
-                        <div className='flex col-span-12 lg:col-span-4 flex-col gap-y-4 w-[200px]'>
-                            <h2 className='flex items-center gap-x-2'>
-                                <BookOpenIcon className='w-4 h-4 text-slate-500' />
-                                References
-                            </h2>
-                            <ul className='text-slate-900 font-light text-sm flex flex-col gap-y-2'>
-                                {agentState?.answer?.references?.map(
-                                    (ref: any, idx: number) => (
-                                        <li key={idx}>
-                                            <a
-                                                href={ref.url}
-                                                target='_blank'
-                                                rel='noopener noreferrer'
-                                            >
-                                                {idx + 1}. {ref.title}
-                                            </a>
-                                        </li>
-                                    )
-                                )}
-                            </ul>
-                        </div>
-                    )}
-                </div>
+        <div className="grid grid-cols-12 gap-8">
+          <div className="col-span-12 lg:col-span-8 flex flex-col">
+            <h2 className="flex items-center gap-x-2">
+              {isLoading ? (
+                <LoaderCircleIcon className="animate-spin w-4 h-4 text-slate-500" />
+              ) : (
+                <SparkleIcon className="w-4 h-4 text-slate-500" />
+              )}
+              Answer
+            </h2>
+
+            <div className="text-slate-500 font-light">
+              {isLoading ? (
+                // (
+                //     <SkeletonLoader />
+                // )
+                "Loading...."
+              ) : (
+                <AnswerMarkdown markdown={agentState?.answer?.markdown} /> // displays search results
+              )}
             </div>
-        </motion.div>
-    );
+          </div>
+
+          {agentState?.answer?.references?.length && (
+            <div className="flex col-span-12 lg:col-span-4 flex-col gap-y-4 w-[200px]">
+              <h2 className="flex items-center gap-x-2">
+                <BookOpenIcon className="w-4 h-4 text-slate-500" />
+                References
+              </h2>
+              <ul className="text-slate-900 font-light text-sm flex flex-col gap-y-2">
+                {agentState?.answer?.references?.map(
+                  (ref: any, idx: number) => (
+                    <li key={idx}>
+                      <a
+                        href={ref.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {idx + 1}. {ref.title}
+                      </a>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div> */}
+      <div className="w-full flex-col my-8 ">
+        <div className="space-y-4">
+          <h1 className="text-3xl lg:text-4xl font-extralight">
+            {researchQuery}
+          </h1>
+        </div>
+        <Tabs defaultValue={tabs[0].value} className="max-w-xs w-full mt-2">
+          <TabsList className="w-full p-0 bg-background justify-start border-b rounded-none">
+            {tabs.map((tab) => (
+              <TabsTrigger
+                key={tab.value}
+                value={tab.value}
+                className="rounded-none flex  bg-background h-full data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-red-500 mb-4"
+              >
+                <code className="text-[20px]">{tab.name}</code>
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
+          <TabsContent key={"results"} value="results">
+            <div>
+              <h1>results</h1>
+              <AnswerMarkdown markdown={agentState?.answer?.markdown} />
+            </div>
+          </TabsContent>
+          <TabsContent value="sources">
+            <div>
+              <h1>sources</h1>
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 }
